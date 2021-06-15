e4e25cfee90fa85c33c80573c84ea32113d2a6e8
NataliaToyama <nahtoyama@hotmail.com>
2021-06-14 23:24:58 -0300

    Projeto Clone Inicial NETFLIX

---
+++ README.md
@@ -0,0 +1,12 @@
+# **Recriação Inicial da NETFLIX**  :baby:
+
+
+
+
+
+##### Olá!  :black_heart:
+
+##### Recrie a interface da NETFLIX customizando para DCFLIX utilizando tecnologias simples como HTML5, CSS3 e JavaScript. 
+
+##### Utilizei layout, técnicas de CSS3 com containers e variáveis,  Flexbox e  plugins Jquery
+
---
+++ js/owl/function.js
@@ -0,0 +1,12 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+}
+   
+function trocar(elemento){
+    elemento.innerHTML = " ";
+}
+ 
+function redirecionar(){
+    window.open("https://www.youtube.com/watch?v=DlaQv86DDUM&list=PLYEAzxPfkPBtNn6j44Xuo50EX0qBgKkNV");
+}
+
